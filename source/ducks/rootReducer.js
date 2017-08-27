7774ac6bbf139ef56c0df2105a42f43ee5730ef9
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-08-27 23:47:55 +0300

    connected router added

--- source/ducks/rootReducer.js
+++ source/ducks/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 import Articles from "./modules/Articles";
 import Article from "./modules/Article";
 
 export default combineReducers({
     Articles,
-    Article
+    Article,
+    routerReducer
 });
