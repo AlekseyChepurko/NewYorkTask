49ed9f46197588e82f787ce4c00f608c5bb8aa9d
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-08-27 20:27:47 +0300

    commas refactor

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import path from 'path';
-import fs from 'fs';
-import compression from 'compression';
+import express from "express";
+import path from "path";
+import fs from "fs";
+import compression from "compression";
 import * as db from "./utils/DBUtils";
 
 const PORT = process.argv[2] || 3000;
@@ -9,24 +9,24 @@ const PORT = process.argv[2] || 3000;
 db.setupConnection();
 const app = express();
 app.use(compression({
-    filter: (req) => !(req.query.compress === 'false'),
+    filter: (req) => !(req.query.compress === "false"),
 }));
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, "../build")));
 
 // Healthchec
-app.get('/health', (req, res) => {
-    fs.readFile('package.json', 'utf8', (err, PackageInfo) => {
+app.get("/health", (req, res) => {
+    fs.readFile("package.json", "utf8", (err, PackageInfo) => {
         if (err) {
             return res.json({
-                status: 'Error, can\'t read package.json',
+                status: "Error, can\"t read package.json",
             });
         }
         const packageJSON = JSON.parse(PackageInfo);
 
         res.json({
             version: packageJSON.version,
-            status: 'OK',
+            status: "OK",
         });
 
         return null;
@@ -52,8 +52,8 @@ app.get("/api/articles/list", async (req, res) => {
 });
 
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
 app.listen(PORT, () => console.info(`Listening on ${PORT} port`));
