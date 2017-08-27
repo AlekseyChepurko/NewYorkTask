368c0caac7b5a6a2dbe48d2466fd3f22b68f3815
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-08-27 20:29:14 +0300

    serch and results components

---
+++ source/components/MainPage/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ArticleSearch from './ArticleSearch';
+import Results from './ArticleSearch/Results';
+
+
+export const MainPage = (props) => <div>
+    <ArticleSearch/>
+    <Results />
+</div>;
