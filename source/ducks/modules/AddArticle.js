f3ecda981e1a2bdb3a4e43efab1966e9a176001c
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-08-28 09:37:45 +0300

    add article reducer

---
+++ source/ducks/modules/AddArticle.js
@@ -0,0 +1,52 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import SagasManager from '../helpers/sagasManager';
+
+const appName = APP_NAME;
+const ADD_ARTICLE = `${appName}/AddArticle/ФВВ_ARTICLE`;
+const ADD_FINISHED = `${appName}/AddArticle/ADD_FINISHED`;
+
+const initialState = {
+    status: ""
+};
+
+export default function AddArticle(state=initialState, action){
+    switch(action.payload){
+        case ADD_FINISHED: return {
+            ...state,
+            ...payload
+        };
+        default: return state;
+    }
+}
+
+export const addArticle = (payload) => ({
+    type: ADD_ARTICLE,
+    payload
+});
+
+const addFinished = (payload) => ({
+    type: ADD_FINISHED,
+    payload
+});
+
+function* watchAddArticle(action){
+    console.log(action);
+    const body = JSON.stringify(action.payload);
+    const res = yield fetch(`/api/articles/add`,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: body,
+        }).then(r => {
+        return r;
+    })
+        .catch(e => {e});
+
+    yield put(addFinished(res))
+}
+
+SagasManager.addSagaToRoot(function* () {
+    yield takeLatest(ADD_ARTICLE, watchAddArticle);
+});
--- source/ducks/rootReducer.js
+++ source/ducks/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import AddArticle from "./modules/AddArticle";
 import Articles from "./modules/Articles";
 import Article from "./modules/Article";
 
 export default combineReducers({
+    AddArticle,
     Articles,
     Article,
     routerReducer
